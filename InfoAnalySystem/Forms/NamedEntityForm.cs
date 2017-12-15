8fc018d61c993283617de1d5d51b6de5a30c494d
zry <zry@DESKTOP-MCUDE8G>
2017-12-15 13:24:04 +0800

    完善命名实体的数据库保存

--- InfoAnalySystem/Forms/NamedEntityForm.cs
+++ InfoAnalySystem/Forms/NamedEntityForm.cs
@@ -21,6 +21,11 @@ namespace InfoAnalySystem.Forms {
         public NamedEntityForm() {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 进行命名实体标记
+        /// </summary>
+        /// <param name="newsId"></param>
         public void doNamedEntityRecognition(int newsId) {
             this.newsId = newsId;
             saveBtn.Visible = true;
@@ -30,52 +35,53 @@ namespace InfoAnalySystem.Forms {
                 c.Dispose();
             }
             News news = DBHelper.db.Queryable<News>().InSingle(newsId);
-            //如果已解析过此新闻
-            if (DBHelper.db.Queryable<Section>().Where(it => it.newsId == news.id).Count() > 0) {
-                sectionList = DBHelper.db.Queryable<Section>().Where(it => it.newsId == news.id).ToList();
-                foreach(var section in sectionList) {
-                    splitEntity(section);
-                }
-            //如果未解析过此新闻
-            }else {
-                var segmenter = new JiebaSegmenter();
-                string[] sections = news.content.Split(' ');
-                int secIndex = 0;
-                foreach (string sectionValue in sections) {
-                    var section = new Section();
-                    section.newsId = news.id;
-                    section.indexInNews = secIndex;
-                    section.value = sectionValue;
-                    sectionList.Add(section);
-                    secIndex += sectionValue.Length+1;
-                    splitEntity(section);
-                }
+            var segmenter = new JiebaSegmenter();
+            var posSeg = new PosSegmenter();
+            // 将新闻按段落分组
+            string[] sections = news.content.Split(' ');
+            int secIndex = 0;
+            foreach (string sectionValue in sections) {
+                var tokens = posSeg.Cut(sectionValue);
+                // 生成实例存入sectionList,用于存入数据库
+                var section = new Section();
+                section.newsId = news.id;
+                section.indexInNews = secIndex;
+                section.value = sectionValue;
+                sectionList.Add(section);
+                // 识别命名实体
+                splitEntity(secIndex, tokens);
+                // 下一section在新闻中的位置
+                secIndex += tokens.Count();
             }
-            
         }
-        private void splitEntity(Section section) {
+
+        /// <summary>
+        /// 对section进行命名实体识别
+        /// </summary>
+        /// <param name="sectionIndex">section在新闻中的位置</param>
+        /// <param name="tokens">section中词--词性pair的集合</param>
+        private void splitEntity(int sectionIndex, IEnumerable<Pair> tokens) {
             string sentence = "";
             int entityFlag = -1;
-            var posSeg = new PosSegmenter();
-            var s = section.value;
-            var tokens = posSeg.Cut(s);
-            int wordIndex = 0;
+            int wordIndex = -1;
             foreach (Pair word in tokens) {
                 int wordFlag = Array.IndexOf(Const.entityList, word.Flag);
                 if (sentence != "") {
                     if (entityFlag == wordFlag) {
                         sentence += word.Word;
                         wordIndex++;
-                    } else {  //找到entity
+                    } else {
                         addLabel(sentence, entityFlag);
+                        if (entityFlag >= 0) { //找到entity
+                            var entity = new NamedEntity();
+                            entity.indexInSection = wordIndex;
+                            entity.indexInNews = wordIndex + sectionIndex;
+                            entity.newsId = this.newsId;
+                            entity.value = sentence;
+                            entityList.Add(entity);
+                        }
                         entityFlag = wordFlag;
                         sentence = word.Word;
-                        var entity = new NamedEntity();
-                        entity.indexInSection = wordIndex;
-                        entity.indexInNews = wordIndex + section.indexInNews;
-                        entity.newsId = this.newsId;
-                        entity.value = word.Word;
-                        entityList.Add(entity);
                         wordIndex++;
                     }
                 } else {
@@ -88,6 +94,12 @@ namespace InfoAnalySystem.Forms {
                 addLabel(sentence, entityFlag);
             }
         }
+
+        /// <summary>
+        /// 在界面加label进行展示
+        /// </summary>
+        /// <param name="sentence">要展示的内容</param>
+        /// <param name="flag">是否为命名实体</param>
         private void addLabel(string sentence, int flag) {
             if (flag >= 0) {
                 LinkLabel titleLabel = new LinkLabel();
@@ -104,20 +116,28 @@ namespace InfoAnalySystem.Forms {
 
         }
 
+        // 命名实体点击函数,跳转到网址
         private static void jumpWindows(object sender, EventArgs e) {
             LinkLabel titleLabel = sender as LinkLabel;
             String sentence = titleLabel.Text;
             System.Diagnostics.Process.Start("https://www.baidu.com/s?ie=utf-8&wd=" + sentence);
         }
 
+        // 存入数据库响应函数
         private void saveBtn_Click(object sender, EventArgs e) {
-            if (DBHelper.db.Queryable<Section>().Where(it => it.newsId == this.newsId).Count() == 0) {
-                DBHelper.db.Insertable(sectionList.ToArray()).ExecuteCommand();
+            bool isExit = DBHelper.db.Queryable<Section>().Where(it => it.newsId == this.newsId).First() != null;
+            bool isOverwrite = true;
+            if (isExit) {
+                DialogResult result = MessageBox.Show("数据库中已有数据，确定覆盖吗？", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+                isOverwrite = result == DialogResult.OK;
             }
-            if (DBHelper.db.Queryable<NamedEntity>().Where(it => it.newsId == this.newsId).Count() == 0) {
+            if(!isExit || isExit && isOverwrite) {
+                DBHelper.db.Deleteable<Section>().Where(it => it.newsId == this.newsId).ExecuteCommand();
+                DBHelper.db.Deleteable<NamedEntity>().Where(it => it.newsId == this.newsId).ExecuteCommand();
+                DBHelper.db.Insertable(sectionList.ToArray()).ExecuteCommand();
                 DBHelper.db.Insertable(entityList.ToArray()).ExecuteCommand();
                 //更新entity对应的sectionId
-                var updateSql = 
+                var updateSql =
                     "UPDATE NamedEntity " +
                     "SET sectionId = ( " +
                         "SELECT id from Section " +
@@ -126,8 +146,10 @@ namespace InfoAnalySystem.Forms {
                     ") " +
                     "where NamedEntity.newsId == @newsId";
                 DBHelper.db.Ado.ExecuteCommand(updateSql, new SqlSugar.SugarParameter("@newsId", this.newsId));
+                MessageBox.Show("保存完成");
             }
-            MessageBox.Show("保存完成");
         }
+
+
     }
 }
