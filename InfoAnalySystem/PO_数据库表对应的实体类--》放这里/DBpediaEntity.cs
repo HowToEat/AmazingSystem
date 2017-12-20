63439bd5b3154848d7652a9a35c5d3a01935f0af
zry <zry@DESKTOP-MCUDE8G>
2017-12-20 19:06:34 +0800

    添加DBpedia实体类

---
+++ InfoAnalySystem/PO_数据库表对应的实体类--》放这里/DBpediaEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoAnalySystem.PO {
+    class DBpediaEntity {
+        public int? id { get; set; }
+        public string rootValue { get; set; }
+        public string value { get; set; }
+    }
+}
