f1301a2a2ffae6c223efc7ca83ae3bb5f60dd3bd
zry <zry@DESKTOP-MCUDE8G>
2017-12-21 11:55:56 +0800

    加入 Event表

---
+++ InfoAnalySystem/PO_数据库表对应的实体类--》放这里/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoAnalySystem.PO {
+    class Event {
+        public int? id { get; set; }
+        public int? newsId { get; set; }
+        public DateTime time { get; set; }
+        public string location { get; set; }
+        public string country { get; set; }
+        public string target { get; set; }
+        public string comment { get; set; }
+        public string topic { get; set; }
+
+    }
+}
