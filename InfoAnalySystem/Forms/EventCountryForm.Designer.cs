591240971ecf0f25a4b69c06851cef7fd77ee4e4
zry <zry@DESKTOP-MCUDE8G>
2017-12-28 10:04:32 +0800

    完成 事件国家界面

--- InfoAnalySystem/Forms/EventCountryForm.Designer.cs
+++ InfoAnalySystem/Forms/EventCountryForm.Designer.cs
@@ -23,18 +23,30 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.webKitBrowser1 = new WebKit.WebKitBrowser();
+            this.mapWebkitBrowser = new WebKit.WebKitBrowser();
+            this.listWebkitBrowser = new WebKit.WebKitBrowser();
             this.SuspendLayout();
             // 
-            // webKitBrowser1
+            // mapWebkitBrowser
             // 
-            this.webKitBrowser1.BackColor = System.Drawing.Color.White;
-            this.webKitBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webKitBrowser1.Location = new System.Drawing.Point(0, 0);
-            this.webKitBrowser1.Name = "webKitBrowser1";
-            this.webKitBrowser1.Size = new System.Drawing.Size(835, 631);
-            this.webKitBrowser1.TabIndex = 0;
-            this.webKitBrowser1.Url = null;
+            this.mapWebkitBrowser.BackColor = System.Drawing.Color.White;
+            this.mapWebkitBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapWebkitBrowser.Location = new System.Drawing.Point(410, 0);
+            this.mapWebkitBrowser.Name = "mapWebkitBrowser";
+            this.mapWebkitBrowser.Size = new System.Drawing.Size(425, 631);
+            this.mapWebkitBrowser.TabIndex = 0;
+            this.mapWebkitBrowser.Url = null;
+            // 
+            // listWebkitBrowser
+            // 
+            this.listWebkitBrowser.BackColor = System.Drawing.Color.White;
+            this.listWebkitBrowser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listWebkitBrowser.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listWebkitBrowser.Location = new System.Drawing.Point(0, 0);
+            this.listWebkitBrowser.Name = "listWebkitBrowser";
+            this.listWebkitBrowser.Size = new System.Drawing.Size(410, 631);
+            this.listWebkitBrowser.TabIndex = 1;
+            this.listWebkitBrowser.Url = null;
             // 
             // EventCountryForm
             // 
@@ -42,7 +54,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(835, 631);
-            this.Controls.Add(this.webKitBrowser1);
+            this.Controls.Add(this.mapWebkitBrowser);
+            this.Controls.Add(this.listWebkitBrowser);
             this.Name = "EventCountryForm";
             this.Text = "EventCountry";
             this.ResumeLayout(false);
@@ -51,6 +64,7 @@
 
         #endregion
 
-        private WebKit.WebKitBrowser webKitBrowser1;
+        private WebKit.WebKitBrowser mapWebkitBrowser;
+        private WebKit.WebKitBrowser listWebkitBrowser;
     }
 }
